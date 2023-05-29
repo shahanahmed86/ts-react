40590aceae18627b320d74d9811794084b9acc60
shahan <shahan.khaan@gmail.com>
2023-05-30 01:52:42 +0500

    using husky previous version

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Home from './pages/Home.page';
 import NotFound from './pages/NotFound.page';
 
 export function App() {
+  // alert('here...');
   return (
     <Routes>
       <Route path="/" element={<Home />} />
