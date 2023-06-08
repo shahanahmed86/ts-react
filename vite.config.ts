1657a8a3c5b8004d920055d31df8e6f65393f2e0
shahan <shahan.khaan@gmail.com>
2023-06-08 14:55:23 +0500

    build: base url for gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'http://shahanahmed86.github.io/ts-react',
+  base: 'https://shahanahmed86.github.io/ts-react/',
   plugins: [react()],
   test: {
     globals: true,
