c8b61fd1f245240b58883ecdb41a741cadce197b
shahan <shahan.khaan@gmail.com>
2023-06-12 12:50:47 +0500

    style: set font-family to button

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,10 +1,11 @@
 import { Button, ButtonProps } from 'react-bootstrap';
 import styled, { css } from 'styled-components';
-import { TTheme } from '../../infrastructure/theme';
+import { TTheme } from '../../app/theme/default';
 
 const defaultTextStyles = (theme: TTheme) => css`
   background-color: ${theme.colors.ui.primary.main};
   color: ${theme.colors.text.primary};
+  font-family: ${theme.fonts.body};
   border-width: 0px;
   outline-width: 10px;
   position: relative;
