eaa0811cac343b003b2345df9395842fc832c92b
shahan <shahan.khaan@gmail.com>
2023-06-12 12:56:03 +0500

    revert: base property

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://shahanahmed86.github.io/ts-react/',
   plugins: [react()],
   test: {
     globals: true,
